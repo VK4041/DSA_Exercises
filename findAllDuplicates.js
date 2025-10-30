afe747fb829b94c5edde2457d89b6a8d85c52eb5
VK4041 <151887513+VK4041@users.noreply.github.com>
2025-10-30 16:41:11 +1100

    Complete findAllDuplicates solution

--- findAllDuplicates.js
+++ findAllDuplicates.js
@@ -4,3 +4,17 @@
 // Note that you can return the elements in any order.
 
 // Time Complexity - O(n)
+function findAllDuplicates(array) {
+  let duplicates = [];
+  let dictionary = {};
+  for (let value of array) {
+    //If value if encounter twice, add to return array and skip iteration
+    if (dictionary[value]) {
+      duplicates.push(value);
+      continue;
+    }
+    dictionary[value] = 1;
+  }
+  return duplicates;
+}
+console.log(findAllDuplicates([2, 3, 4, 2, 7, 6, 4]));
